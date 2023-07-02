daedfb05a11c1e6146f41f84000c05d716a383e7
KafuiAdaku <dennisadaku@ymail.com>
2023-07-02 10:23:46 -0700

    end of file error corrected on task 9

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -24,7 +24,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, (int, float):
+        if not isinstance(value, (int, float)):
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')

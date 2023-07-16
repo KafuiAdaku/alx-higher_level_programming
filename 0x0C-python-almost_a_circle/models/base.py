9a01fc51a6ad116ef205bdffae56d6f9d9af4522
KafuiAdaku <dennisadaku@ymail.com>
2023-07-16 07:59:18 -0700

    task 15: updated :def to_json_string(list_dictionaries); task 16: updated : def save_to_file(cls, list_objs)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """My base module"""
+import json
 
 
 class Base:
@@ -18,3 +19,39 @@ class Base:
         if not id:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            returns the JSON string representation of list_dictionaries
+
+            Args:
+                list_dictionaries (lsit): a list of dictionaries
+
+            returns: a json string
+        """
+        if list_dictionaries == None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+             writes the JSON string representation of list_objs to a file
+
+             Args:
+                cls (class): class of object
+                list_objs (list):  list of instances that inherit from Base
+        """
+        filename = cls.__name__ + ".json"
+        my_list = []
+        if list_objs is None:
+            with open(filename,'w', encoding='utf-8') as f:
+                pass
+
+        if list_objs is not None:
+            for obj in list_objs:
+                my_list.append(obj.to_dictionary())
+
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(cls.to_json_string(my_list))

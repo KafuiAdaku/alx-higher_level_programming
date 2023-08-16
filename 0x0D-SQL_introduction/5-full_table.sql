e1fb7cd15a223b18cdb9805f405dae3387ae408d
Kafui Adaku <dennisadaku@ymail.com>
2023-08-16 06:46:04 -0700

    added solution to task 6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- database hbtn_0c_0 in your MySQL server.
 -- * The database name will be passed as an argument of the mysql command
 -- * You are not allowed to use the DESCRIBE or EXPLAIN statements
-SHOW CREATE TABLE first_table
+SHOW CREATE TABLE first_table;

b638f0bfcad084fba9351cc2830e085fdd005d1b
Kafui Adaku <dennisadaku@ymail.com>
2023-08-17 04:50:53 -0700

    added solution to task 17: code error correction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,6 +6,7 @@
 --	* Field name in first_table
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn+0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET
 utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256)

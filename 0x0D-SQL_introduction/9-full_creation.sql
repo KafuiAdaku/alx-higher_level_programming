d298b06e87290713f9f0fbdb3ff05424b17cbf6b
Kafui Adaku <dennisadaku@ymail.com>
2023-08-17 05:05:41 -0700

    code error correction task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@
 -- The database name will be passed as an argument to the mysql command
 -- If the table second_table already exists, your script should not fail
 -- You are not allowed to use the SELECT and SHOW statements
---Your script should create these records:
+-- Your script should create these records:
 --	* id = 1, name = “John”, score = 10
 --	* id = 2, name = “Alex”, score = 3
 --	* id = 3, name = “Bob”, score = 14

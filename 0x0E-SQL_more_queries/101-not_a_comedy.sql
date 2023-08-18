c7836092fc50f7d742f9a42540be39255ac4d59e
Kafui Adaku <dennisadaku@ymail.com>
2023-08-18 15:38:41 -0700

    added solution to task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -13,6 +13,6 @@ WHERE title NOT IN (
 	INNER JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 	INNER JOIN tv_genres
-	ON tv_show_genres.genre_id = tv_genre.id
-	WHERE tv_genre.name = "Comedy")
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = "Comedy")
 ORDER BY title ASC;

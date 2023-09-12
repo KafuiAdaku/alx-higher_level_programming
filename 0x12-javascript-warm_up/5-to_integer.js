d99ecd3de2df43b13a6a8a832b84f850615a59a5
Kafui Adaku <dennisadaku@ymail.com>
2023-09-12 09:38:06 -0700

    added solution to task 5: code style implementation

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (Number(process.argv[2])) {
-	// convert float to integer
+  // convert float to integer
   console.log(`My number: ${Number(process.argv[2]) | 0}`);
 } else {
   console.log('Not a number');

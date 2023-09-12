fb5082d49929675fdd1ceda9a508bcc06c5e10f5
Kafui Adaku <dennisadaku@ymail.com>
2023-09-12 16:23:46 -0700

    added solution to task 3: code style correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,15 +10,15 @@ module.exports = class Rectangle {
   }
 
   print () {
-  let square = '';
-  for (let height = 0; height < this.height; height++) {
-    for (let width = 0; width < this.width; width++) {
-      square += 'X';
+    let square = '';
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        square += 'X';
+      }
+      if (height !== this.height - 1) {
+        square += '\n';
+      }
     }
-    if (height !== this.height - 1) {
-      square += '\n';
-    }
-  }
     if (square) {
       console.log(square);
     }

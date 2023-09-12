7d3a4313deedd48edb084421a9da772734672135
Kafui Adaku <dennisadaku@ymail.com>
2023-09-12 16:33:34 -0700

    added solution to task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
+      return ('Rectangle {}');
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let square = '';
+    for (let height = 0; height < this.height; height++) {
+      for (let width = 0; width < this.width; width++) {
+        square += 'X';
+      }
+      if (height !== this.height - 1) {
+        square += '\n';
+      }
+    }
+    if (square) {
+      console.log(square);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};

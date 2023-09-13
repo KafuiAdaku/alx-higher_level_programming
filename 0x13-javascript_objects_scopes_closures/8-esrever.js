2a294ba37617b9b2eb4e3333db99a6d4ee5b9980
Kafui Adaku <dennisadaku@ymail.com>
2023-09-12 17:50:11 -0700

    task 8: refactored code- requirements did not allow usage of built-in fucntions

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const reversed = [];
+  let revIdx = list.length - 1;
+  let index = 0;
+
+  for (index, revIdx; index < list.length; index++, revIdx--) {
+    reversed[index] = list[revIdx];
+  }
+  return (reversed);
 };

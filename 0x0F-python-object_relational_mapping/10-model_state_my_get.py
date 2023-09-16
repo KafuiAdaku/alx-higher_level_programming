bb917a2fbde08eb1f7a305d65cd5c1b4d1b6d999
Kafui Adaku <dennisadaku@ymail.com>
2023-09-16 08:28:55 -0700

    added solution to task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""a script that lists all State objects that contain the
-    letter `a` from the database` hbtn_0e_6_usa`.
-    Your script should take 3 arguments: `mysql username`,
-    `mysql password` and `database name`.
+"""a script that prints the State object with the
+    name passed as argument from the database `hbtn_0e_6_usa`.
+    Your script should take 4 arguments: `mysql username`, `mysql password`,
+    `database name` and `state name to search` (SQL injection free).
     You must use the module SQLAlchemy.
-    You must import State and Base from `model_state` -
+    You must import State and Base from model_state -
     `from model_state import Base, State`.
     Your script should connect to a MySQL server running on
     localhost at port 3306.
-    Results must be sorted in ascending order by `states.id`
-    The results must be displayed as they are in the example below
+    You can assume you have one record with the state name to search.
+    Results must display the states.id.
+    If no state has the name you searched for, display `Not found`
     Your code should not be executed when imported
 """
 if __name__ == "__main__":
@@ -22,21 +23,25 @@ if __name__ == "__main__":
     parser.add_argument("mysql_username", type=str, help="MySQL username")
     parser.add_argument("mysql_password", type=str, help="MySQL password")
     parser.add_argument("database", type=str, help="database name")
+    parser.add_argument("state_name", type=str, help="state name to search")
 
     args = parser.parse_args()
     username = args.mysql_username
     password = args.mysql_password
     database = args.database
+    state_name = args.state_name
 
     engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:\
             3306/{database}")
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.ilike("%a%")).\
-        order_by(asc(State.id)).all()
+    states = session.query(State).filter_by(name=state_name).\
+        order_by(asc(State.id)).first()
 
-    for state in states:
-        print(f"{state.id}: {state.name}")
+    if states:
+        print(f"{states.id}")
+    else:
+        print("Not found")
 
     session.close()

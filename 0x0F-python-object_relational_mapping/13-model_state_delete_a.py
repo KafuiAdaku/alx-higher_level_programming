7a9bdd0fb63833d6d98dc74b850d7ecf695bafb8
Kafui Adaku <dennisadaku@ymail.com>
2023-09-16 09:17:04 -0700

    added solution to task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a script that changes the name of a `State` object
-    from the database `hbtn_0e_6_usa`.
+""" a script that deletes all `State` objects with a name
+    containing the letter `a` from the database `hbtn_0e_6_usa`.
     Your script should take 3 arguments: `mysql username`,
     `mysql password` and `database name`.
     You must use the module SQLAlchemy.
@@ -8,7 +8,6 @@
     `from model_state import Base, State`.
     Your script should connect to a MySQL server running on
     localhost at port 3306.
-    Change the name of the `State` where `id = 2` to `New Mexico`.
     Your code should not be executed when imported
 """
 if __name__ == "__main__":
@@ -32,8 +31,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter_by(id="2").first()
-    states.name = "New Mexico"
+    states_to_del = session.query(State).filter(State.name.ilike("%a%")).all()
 
-    session.commit()
+    if states_to_del:
+        for state in states_to_del:
+            session.delete(state)
+        session.commit()
+
     session.close()

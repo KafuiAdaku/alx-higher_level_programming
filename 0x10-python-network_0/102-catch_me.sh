b333f7fc5d34870cc33098a9973631a29c249a34
KafuiAdaku <dennisadaku@ymail.com>
2023-09-30 15:47:08 -0700

    task 9: refactored code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to 0.0.0.0:5000/catch_me to trigger the desired response
-curl -s 0.0.0.0:5000/catch_me | grep -o "You got me!"
+curl -s -X PUT -L -d "user_id=98" "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool"

d3f12014d8e0feab516e3ad5007ed95fc8e74aee
KafuiAdaku <dennisadaku@ymail.com>
2023-09-30 15:45:14 -0700

    task 9: refactored code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to 0.0.0.0:5000/catch_me to trigger the desired response
-curl -s -X PUT -L -d "user_id:001" "0.0.0.0/catch_me"
+curl -s 0.0.0.0:5000/catch_me | grep -o "You got me!"

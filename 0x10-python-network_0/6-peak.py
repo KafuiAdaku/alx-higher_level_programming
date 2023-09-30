90ba9ecce0d9c69f6ded2ebbffcee7614e0a36b4
KafuiAdaku <dennisadaku@ymail.com>
2023-09-30 15:10:42 -0700

    task 6: refactored code for efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,17 +14,35 @@
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
     if not isinstance(list_of_integers, list):
-        return
-    peak = []
-    for idx in range(len(list_of_integers)):
-        if idx == 0:
-            if list_of_integers[idx] >= list_of_integers[idx + 1]:
-                peak.append(list_of_integers[idx])
-        elif idx == len(list_of_integers) - 1:
-            if list_of_integers[idx] >= list_of_integers[idx - 1]:
-                peak.append(list_of_integers[idx])
+        return None
+    if not list_of_integers:
+        return None
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
         else:
-            if list_of_integers[idx] >= list_of_integers[idx - 1] and\
-                    list_of_integers[idx] >= list_of_integers[idx + 1]:
-                peak.append(list_of_integers[idx])
-    return max(peak) if peak else None
+            high = mid
+    return list_of_integers[low]
+
+
+# def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+#     if not isinstance(list_of_integers, list):
+#         return None
+#     peak = []
+#     for idx in range(len(list_of_integers)):
+#         if idx == 0:
+#             if list_of_integers[idx] >= list_of_integers[idx + 1]:
+#                 peak.append(list_of_integers[idx])
+#         elif idx == len(list_of_integers) - 1:
+#             if list_of_integers[idx] >= list_of_integers[idx - 1]:
+#                 peak.append(list_of_integers[idx])
+#         else:
+#             if list_of_integers[idx] >= list_of_integers[idx - 1] and\
+#                     list_of_integers[idx] >= list_of_integers[idx + 1]:
+#                 peak.append(list_of_integers[idx])
+#     return max(peak) if peak else None

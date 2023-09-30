6cb546294618caad3b5ce39216f66837b6ff0c2c
KafuiAdaku <dennisadaku@ymail.com>
2023-09-30 08:06:49 -0700

    code correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # sends a request, and displays the size of the body of the response 
-curl -s "$1" |awk -F ": "'/Content-Length/ {print $2}'
+curl -s "$1" |awk '/Content-Length/ {print $2}'
